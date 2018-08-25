16ae0e4e85ed540caf4378dfde16407e680b5cb5
ipeeddy <ipeeddy@gmail.com>
2018-08-25 12:07:23 +0700

    Morning 2018-08-25

--- app/src/main/java/masterung/androidthai/in/th/sutfriend/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/sutfriend/RegisterFragment.java
@@ -4,12 +4,43 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolba
+        createToolba();
+
+
+    } // Main Method
+
+    private void createToolba() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity)getActivity()).getSupportActionBar().setSubtitle(getString(R.string.descrip));
+
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
